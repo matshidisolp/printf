0c047c2cf1f846077888634582ca43c1a010367f
matshidisolp <mpitswane@gmail.com>
2023-07-25 15:11:34 -0700

    Going in circles with edge cases

--- test/_printf.c
+++ test/_printf.c
@@ -1,9 +1,5 @@
 #include "main.h"
-/**
- * _printf - Produces output according to a format.
- * @format: a character string
- * Return: number of characters printed excl. null-terminator
- */
+
 int _printf(const char *format, ...)
 {
 	int printed_char = 0;
@@ -11,26 +7,30 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
+	if (!format)
+		return (-1);
+
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format == '\0')
-				break;
-			if (*format == 'c')
-				printed_char += print_char(va_arg(args, int));
-			else if (*format == 's')
-				printed_char += print_string(va_arg(args, char*));
-			else if (*format == 'd' || *format == 'i')
-				printed_char += print_number(va_arg(args, int));
-			else
+			if (*format == '%')
 				printed_char += _putchar(*format);
+			else if (*format == 'c')
+				printed_char += _putchar(va_arg(args, int));
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+
+				while (str && *str)
+					printed_char += _putchar(*str++);
+			}
+			else
+				printed_char += (*format ? (_putchar('%') + _putchar(*format)) : -1);
 		}
 		else
-		{
 			printed_char += _putchar(*format);
-		}
 		format++;
 	}
 	va_end(args);
--- test/main.c
+++ test/main.c
@@ -1,7 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - Entry point
  *
@@ -9,35 +8,22 @@
  */
 int main(void)
 {
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
+	int num1, num2;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
-	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -762534);
-	printf("Negative:[%d]\n", -762534);
-	_printf("Unsigned:[%u]\n", ui);
-	printf("Unsigned:[%u]\n", ui);
-	_printf("Unsigned octal:[%o]\n", ui);
-	printf("Unsigned octal:[%o]\n", ui);
-	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
-	_printf("Address:[%p]\n", addr);
-	printf("Address:[%p]\n", addr);
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
+	num1 = _printf("A %s sentence\n", "simple");
+	num2 = printf("A %s sentence\n", "simple");
+	printf("Len:[%d]\n", num1);
+	printf("Len1:[%d]\n\n", num2);
+	num1 = _printf("A %c%c%s sentence\n", 's', 'i', "mple");
+	num2 = printf("A %c%c%s sentence\n", 's', 'i', "mple");
+	printf("Len:[%d]\n", num1);
+	printf("Len1:[%d]\n\n", num2);
+	num1 = _printf("A simple %v entence\n");
+	printf("Len:[%d]\n", num1);
+	printf("Len1:[%d]\n\n", num2);
+	num1 = _printf("Percentage: %%\n");
+	num2 = printf("Percentage: %%\n");
+	printf("Len:[%d]\n", num1);
+	printf("Len1:[%d]\n\n", num2);
 	return (0);
 }
